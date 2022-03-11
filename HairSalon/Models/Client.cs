0b93040f10ec4b72896d43fcae065824b3fa1649
Liam Eller <liamthelastson@gmail.com>
2022-03-11 12:00:55 -0800

    WIP : debugging unkown bug preventing from accessing database

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,7 +2,9 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    
+    public string Name { get; set; }
+    public int ClientId { get; set; }
 
+    public virtual Client client { get; set; }
   }
 }
